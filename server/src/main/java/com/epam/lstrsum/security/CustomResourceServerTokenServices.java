f0c323eae0655ec17cfeb8b8e221c94832f69acc
Mikhail <figurin_mih@mail.ru>
2017-07-06 19:21:54 +0300

    EPMLSTRSUM-43 Role-based access control added.
    SecurityConfiguration class now looking for RoleService and gets mapping of
    roles and allowed requests to setup the application security.
    
    ResourceBundle implementation of RoleService is provided.
    
    ServerTokenService should set correct role to each user, using RoleService
    also.
    
    Minor fix: *.log added to .gitignore

--- server/src/main/java/com/epam/lstrsum/security/CustomResourceServerTokenServices.java
+++ server/src/main/java/com/epam/lstrsum/security/CustomResourceServerTokenServices.java
@@ -1,5 +1,6 @@
 package com.epam.lstrsum.security;
 
+import com.epam.lstrsum.security.role.RoleService;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.jwt.Jwt;
 import org.springframework.security.jwt.JwtHelper;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
@@ -16,14 +18,19 @@ import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.OAuth2Request;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 
-import java.util.Collections;
 import java.util.Map;
 
 @Slf4j
 public class CustomResourceServerTokenServices implements ResourceServerTokenServices {
 
-    @Autowired
-    AuthorizationCodeResourceDetails authorizationCodeResourceDetails;
+    private final AuthorizationCodeResourceDetails authorizationCodeResourceDetails;
+
+    private final RoleService roleService;
+
+    public CustomResourceServerTokenServices(RoleService roleService, AuthorizationCodeResourceDetails authorizationCodeResourceDetails) {
+        this.roleService = roleService;
+        this.authorizationCodeResourceDetails = authorizationCodeResourceDetails;
+    }
 
     public OAuth2Authentication loadAuthentication(String accessToken) throws AuthenticationException, InvalidTokenException {
 
@@ -39,7 +46,7 @@ public class CustomResourceServerTokenServices implements ResourceServerTokenSer
                     null, null, null, null);
 
             final UsernamePasswordAuthenticationToken finalToken =
-                    new UsernamePasswordAuthenticationToken(principle, "N/A", Collections.emptyList());
+                    new UsernamePasswordAuthenticationToken(principle, "N/A", AuthorityUtils.createAuthorityList(roleService.getPrincipalRoles(principle)));
 
             return new OAuth2Authentication(request, finalToken);
         } catch (Exception e) {
