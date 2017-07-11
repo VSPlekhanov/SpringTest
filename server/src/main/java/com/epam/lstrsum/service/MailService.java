33a62317169286d1b309fc08653e19e6ab0369ad
olvin.hh <olvin.hh@yandex.ru>
2017-07-11 12:12:42 +0300

    EPMLSTRSUM-28 Investigate MS Outlook integration possibilities.Fixed matchesToRegexp method (it returned false at multiline input).

---
+++ server/src/main/java/com/epam/lstrsum/service/MailService.java
@@ -0,0 +1,52 @@
+package com.epam.lstrsum.service;
+
+import com.sun.xml.internal.messaging.saaj.packaging.mime.internet.MimeMultipart;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.integration.annotation.Poller;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.stereotype.Component;
+
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.regex.Pattern;
+
+@Component
+@Profile("production")
+@Slf4j
+public class MailService {
+
+    @ServiceActivator(inputChannel = "receiveChannel", poller = @Poller(fixedRate = "200"))
+    public void showMessages(MimeMessage message) throws Exception {
+        String contentType = message.getContentType();
+
+        log.info(contentType);
+
+        String content = "";
+        if (matchesToRegexp(contentType, "^multipart\\/.*")) {
+            MimeMultipart rawContent = (MimeMultipart) message.getContent();
+            content = (String) rawContent.getBodyPart(0).getContent();
+
+            log.info("mime is multi");
+        } else if (matchesToRegexp(contentType, "^text\\/.*")) {
+            content = (String) message.getContent();
+            log.info("mime is text");
+        } else {
+            log.info("Unknown mime type!");
+        }
+
+        InternetAddress address = (InternetAddress) message.getFrom()[0];
+
+        log.info("Email received ------------------------------------");
+
+        log.info("\nFrom: %s \nSubject: %s \nContent: \n%s",
+                address.getAddress(), message.getSubject(), content);
+
+        log.info("\n\n------------------------------------");
+    }
+
+    private static boolean matchesToRegexp(String input, String regexp){
+        Pattern p = Pattern.compile(regexp);
+        return p.asPredicate().test(input);
+    }
+}
