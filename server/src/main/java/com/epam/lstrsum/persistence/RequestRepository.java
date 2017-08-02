91eedf96f54c3931da1c7a83802864bc47d5005c
Евгений <evgeniy.alexandrov1994@gmail.com>
2017-08-02 06:32:24 +0300

    Push from new local branch. Now EmailParserService return inner EmailForApplication class, that store data for requestPostDto, AnswerPostDto, AttachmentAllFieldsDto.

--- server/src/main/java/com/epam/lstrsum/persistence/RequestRepository.java
+++ server/src/main/java/com/epam/lstrsum/persistence/RequestRepository.java
@@ -2,12 +2,14 @@ package com.epam.lstrsum.persistence;
 
 
 import com.epam.lstrsum.model.Request;
+import com.epam.lstrsum.model.User;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.core.query.TextCriteria;
 import org.springframework.data.mongodb.repository.CountQuery;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RequestRepository extends MongoRepository<Request, String> {
     List<Request> findAllBy(TextCriteria criteria, Pageable pageable);
@@ -18,4 +20,6 @@ public interface RequestRepository extends MongoRepository<Request, String> {
 
     @CountQuery("{$text: {$search: ?0}}")
     int getTextSearchResultsCount(String query);
+
+    Optional<Request> findRequestByTitleAndTextAndAuthorId(String subject, String requestText, User authorId);
 }
