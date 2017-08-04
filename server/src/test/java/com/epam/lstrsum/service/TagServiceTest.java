5cdd3fd3cada8687f2a4365d626bb5e6bb4048d4
Rustam_Kadyrov <Rustam_Kadyrov@epam.com>
2017-08-04 12:39:56 +0300

    Check tags test on env.

--- server/src/test/java/com/epam/lstrsum/service/TagServiceTest.java
+++ server/src/test/java/com/epam/lstrsum/service/TagServiceTest.java
@@ -2,7 +2,7 @@ package com.epam.lstrsum.service;
 
 import com.epam.lstrsum.SetUpDataBaseCollections;
 import com.epam.lstrsum.dto.question.QuestionPostDto;
-import org.junit.Ignore;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.SpyBean;
@@ -17,6 +17,7 @@ import static org.hamcrest.number.OrderingComparison.greaterThanOrEqualTo;
 import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.verify;
 
+@Slf4j
 public class TagServiceTest extends SetUpDataBaseCollections {
     private static final int TAG_COUNT = 20;
     private static final String MOST_POPULAR_TAG = "javascript";
@@ -44,16 +45,18 @@ public class TagServiceTest extends SetUpDataBaseCollections {
     }
 
     @Test
-    @Ignore
+//    @Ignore
     public void getAllTagsCacheWorksOk() {
         List<String> beforeAddTags = tagService.getTagsRating();
         tagService.getTagsRating();
+        log.info("getAllTagsCacheWorksOk; beforeAddTags: {}", beforeAddTags);
 
         final String newTag = "newUniqueTag";
         questionService.addNewQuestion(new QuestionPostDto("title", new String[]{newTag},
                 "textlong", 1L, Collections.singletonList("Bob_Hoplins@epam.com")), "Bob_Hoplins@epam.com");
 
         List<String> afterAddTags = tagService.getTagsRating();
+        log.info("getAllTagsCacheWorksOk; afterAddTags: {}", afterAddTags);
         assertThat(afterAddTags.size(), greaterThan(beforeAddTags.size()));
     }
 }
