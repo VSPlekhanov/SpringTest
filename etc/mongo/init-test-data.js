3cdea1059a8d260f5aca873bdc89acf2eade000e
Aleksandr Shevkunenko <aleksandr_shevkunenko@epam.com>
2017-09-18 18:41:08 +0300

    EPMLSTRSUM-156 Create init-test-data.js. init-test-data.js script done and checked via MongoDB GUI tool and Swagger. Remarks done.

--- etc/mongo/init-test-data.js
+++ etc/mongo/init-test-data.js
@@ -1,6 +1,9 @@
 /*
 * for running scripts use
-*  mongo <host>:<port> mongo.init/init.js
+*  mongo <host>:<port> /etc/mongo/init-test-data.js
+
+* init-test-data.js should generate random data except users.
+* It has to take existing users and use them instead of randomly generated users.
 */
 
 function randomString(length, chars) {
@@ -19,29 +22,12 @@ function randomISODate() {
     return ISODate(new Date(randomInt(1990, 2015), randomInt(1, 10), randomInt(1, 10), randomInt(1, 10), randomInt(1, 10)).toISOString())
 }
 
-function randomEpamEmail() {
-    const userNameMinLength = 2
-    const userNameMaxLength = 15
-    const epamDomainName = '@epam.com'
-    result = ''
-    result += randomString(randomInt(userNameMinLength, userNameMaxLength), EMAIL_CHARS)
-    result += '_';
-    result += randomString(randomInt(userNameMinLength, userNameMaxLength), EMAIL_CHARS)
-    result += epamDomainName;
-    return result
-}
-
 /*ATTENTION!!!!
 * This number response for how many documents will in db after script
 */
-const USERS_AMOUNT_MIN = 50
-const USERS_AMOUNT_MAX = 2000
-const QUESTIONS_AMOUNT_MIN = 500
 const QUESTIONS_AMOUNT_MAX = 1000
 const ANSWER_ON_QUESTION_AMOUNT_MIN = 0
 const ANSWER_ON_QUESTION_AMOUNT_MAX = 20
-const ATTACHMENT_ON_QUESTION_CHAR_AMOUNT_MIN = 102400 / 2  //102400 byte -> 100kb, assume that UTF-16 -> 2 bytes per chars
-const ATTACHMENT_ON_QUESTION_CHAR_AMOUNT_MAX = 16777216 / 2 // 16777216 byte -> 16mb, assume that UTF-16 -> 2 bytes per chars
 /**/
 
 const USER_COLLECTION_NAME = "User"
@@ -54,63 +40,18 @@ const MAXIMUM_TAGS_AMOUNT = 20
 const MINIMUM_TAGS_AMOUNT = 5
 const MINIMUM_TEXT_SIZE = 100
 const MAXIMUM_TEXT_SIZE = 2000
-const ALL_ROLES = ["ROLE_ADMIN", "ROLE_SIMPLE_USER", "ROLE_EXTENDED_USER", "ROLE_NOT_ALLOWED_USER"]
 const GENERAL_CHARS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ      '
-const EMAIL_CHARS = 'abcdefghijklmnopqrstuvwxyz'
 
 collectionNames = db.getCollectionNames()
 
-//clear collections
-for (let i = 0; i < collectionNames.length; ++i) {
-    db.getCollection(collectionNames[i]).remove({})
-}
+//clear Question, Answer and Subscription collections
+db.getCollection(QUESTION_COLLECTION_NAME).remove({})
+db.getCollection(ANSWER_COLLECTION_NAME).remove({})
+db.getCollection(SUBSCRIPTION_COLLECTION_NAME).remove({})
 
-//create Users
-for (let i = 0; i < USERS_AMOUNT_MAX; ++i) {
-    newUser = {}
-    newUser._class = "com.epam.lstrsum.model.User";
-    newUser.firstName = randomString(LITTLE_STRING_LENGTH, GENERAL_CHARS)
-    newUser.lastName = randomString(LITTLE_STRING_LENGTH, GENERAL_CHARS)
-    newUser.email = randomEpamEmail()
-    newUser.isActive = true
-    newUser.roles = []
-    newUser.createdAt = randomISODate()
-    roleAmount = randomInt(0, ALL_ROLES.length)
-
-    for (let j = 0; j < roleAmount; ++j) {
-        newUser.roles.push(ALL_ROLES[randomInt(0, ALL_ROLES.length)])
-    }
-
-    db.getCollection(USER_COLLECTION_NAME).insert(newUser)
-}
-
-db.User.insert({
-    "_class": "com.epam.lstrsum.model.User",
-    "firstName": "John",
-    "lastName": "Doe",
-    "email": "john_doe@epam.com",
-    "roles": [
-        "ROLE_EXTENDED_USER"
-    ],
-    "createdAt": ISODate("2017-09-04T10:49:52.265Z"),
-    "isActive": true
-})
-
+//if the index already exist, this will take no effect
 db.getCollection(USER_COLLECTION_NAME).createIndex({"email": 1}, {"unique": true});
 allUsers = db.getCollection(USER_COLLECTION_NAME).find().toArray()
-
-//create Attachments
-for (let i = 0; i < Math.floor(QUESTIONS_AMOUNT_MAX / 10); ++i) {
-    newAttachment = {}
-    newAttachment._class = "com.epam.lstrsum.model.Attachment";
-    newAttachment.name = randomString(LITTLE_STRING_LENGTH, GENERAL_CHARS)
-    newAttachment.type = randomString(LITTLE_STRING_LENGTH, GENERAL_CHARS)
-    newAttachment.data = randomString(randomInt(ATTACHMENT_ON_QUESTION_CHAR_AMOUNT_MIN, ATTACHMENT_ON_QUESTION_CHAR_AMOUNT_MAX), GENERAL_CHARS)
-
-    db.getCollection(ATTACHMENT_COLLECTION_NAME).insert(newAttachment)
-}
-
-allAttachments = db.getCollection(ATTACHMENT_COLLECTION_NAME).find({}).toArray()
 
 //create Questions
 for (let i = 0; i < QUESTIONS_AMOUNT_MAX; ++i) {
@@ -136,13 +77,9 @@ for (let i = 0; i < QUESTIONS_AMOUNT_MAX; ++i) {
     }
 
     newQuestion.attachmentIds = []
-    if (i % 10 == 0) {
-        newQuestion.attachmentIds.push(allAttachments[randomInt(0, allAttachments.length)]._id.str)
-    }
 
     db.getCollection(QUESTION_COLLECTION_NAME).insert(newQuestion)
 }
-
 
 db.getCollection(QUESTION_COLLECTION_NAME).createIndex({"title": 1, "authorId": 1}, {"unique": true});
 db.getCollection(QUESTION_COLLECTION_NAME).createIndex({"_fts": "text", "_ftsx": 1},
@@ -156,7 +93,7 @@ allQuestions = db.getCollection(QUESTION_COLLECTION_NAME).find({}).toArray()
 
 //create Answers
 allQuestions.forEach(function (question, i, arr) {
-    for (let j = 0; j < randomInt(ANSWER_ON_QUESTION_AMOUNT_MIN, ANSWER_ON_QUESTION_AMOUNT_MAX); ++j) {
+    for (let j = 0, n = randomInt(ANSWER_ON_QUESTION_AMOUNT_MIN, ANSWER_ON_QUESTION_AMOUNT_MAX); j < n; ++j) {
         newAnswer = {}
         newAnswer._class = "com.epam.lstrsum.model.Answer";
         newAnswer.questionId = DBRef(QUESTION_COLLECTION_NAME, question._id)
@@ -174,7 +111,7 @@ db.getCollection(ANSWER_COLLECTION_NAME).createIndex({"questionId": 1});
 allAnswers = db.getCollection(ANSWER_COLLECTION_NAME).find().toArray()
 
 //create Subscriptions
-for (let i = 0; i < USERS_AMOUNT_MAX; ++i) {
+for (let i = 0; i < allUsers.length; ++i) {
     newSubscription = {}
     newSubscription._class = "com.epam.lstrsum.model.Subscription";
     newSubscription.userId = DBRef(USER_COLLECTION_NAME, allUsers[randomInt(0, allUsers.length)]._id)
