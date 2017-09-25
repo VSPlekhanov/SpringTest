6290a7ad20d6d34ee0584e09dd9f8c6fbb45d838
Daria Makarova <Daria_Makarova@epam.com>
2017-09-25 17:37:57 +0300

    feature-EPMLSTRSUM-116-Create-tests-for-load-testing
    -add script and test plan files.

---
+++ etc/performance/init_mongo_localhost_win.cmd
@@ -0,0 +1,3 @@
+cd path\to\mongodb\bin
+mongo localhost:27017/ExperienceDataBase path/to/init.js
+
---
+++ etc/performance/init_mongo_remote_win.cmd
@@ -0,0 +1,2 @@
+cd path\to\mongodb\bin
+mongo ECSE00100A13.epam.com:27017/ExperienceDataBase path\to\init.js
