f4dbe64e02866b40ddc7b9e87e5a4978072736ec
Maksim Nikolaev <Maksim_Nikolaev@epam.com>
2017-09-25 13:54:26 +0300

    EPMLSTRSUM-160 Move rest method GET /api/users to GET /admin/users. Test method getListOfUsers() works without restApi.

--- server/src/test/java/com/epam/lstrsum/controller/AdminControllerTest.java
+++ server/src/test/java/com/epam/lstrsum/controller/AdminControllerTest.java
@@ -1,31 +1,19 @@
 package com.epam.lstrsum.controller;
 
-import com.epam.lstrsum.SetUpDataBaseCollections;
 import com.epam.lstrsum.dto.user.UserBaseDto;
-import com.epam.lstrsum.model.User;
 import com.epam.lstrsum.service.AnswerService;
 import com.epam.lstrsum.service.QuestionService;
 import com.epam.lstrsum.service.UserService;
 import com.epam.lstrsum.service.mail.UserSynchronizer;
 import com.epam.lstrsum.testutils.AssertionUtils;
-import com.google.common.collect.ImmutableList;
-import org.hamcrest.MatcherAssert;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.epam.lstrsum.testutils.InstantiateUtil.EXISTING_QUESTION_ID;
 import static com.epam.lstrsum.testutils.InstantiateUtil.NON_EXISTING_QUESTION_ID;
-import static java.util.stream.Collectors.collectingAndThen;
-import static java.util.stream.Collectors.toList;
+import static com.epam.lstrsum.testutils.InstantiateUtil.someUserBaseDto;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
@@ -33,10 +21,7 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-public class AdminControllerTest extends SetUpDataBaseCollections {
-    @Autowired
-    private TestRestTemplate restTemplate;
-
+public class AdminControllerTest {
     private QuestionService questionService = mock(QuestionService.class);
     private AnswerService answerService = mock(AnswerService.class);
     private UserSynchronizer userSynchronizer = mock(UserSynchronizer.class);
@@ -48,14 +33,14 @@ public class AdminControllerTest extends SetUpDataBaseCollections {
 
     @Test
     public void getListOfUsers() throws Exception {
-        ResponseEntity<List<UserBaseDto>> responseEntity = restTemplate.exchange("/admin/user/list",
-                HttpMethod.GET, null, new ParameterizedTypeReference<List<UserBaseDto>>() {
-                });
-        List<UserBaseDto> actualList = responseEntity.getBody();
-        //validate
-        MatcherAssert.assertThat(actualList.size(), is(7));
-        List<String> actualIds = actualList.stream().map(UserBaseDto::getUserId).collect(collectingAndThen(toList(), ImmutableList::copyOf));
-        MatcherAssert.assertThat(actualIds, containsInAnyOrder("1u", "2u", "3u", "4u", "5u", "6u", "7u"));
+        UserBaseDto userBaseDto = someUserBaseDto();
+        List<UserBaseDto> listUserBaseDto = new ArrayList<>();
+        listUserBaseDto.add(userBaseDto);
+        doReturn(listUserBaseDto).when(userService).findAllUserBaseDtos();
+        List<UserBaseDto> actualList = adminController.getListOfUsers().getBody();
+
+        assertThat(actualList).containsOnly(userBaseDto);
+        verify(userService, times(1)).findAllUserBaseDtos();
     }
 
     @Test
