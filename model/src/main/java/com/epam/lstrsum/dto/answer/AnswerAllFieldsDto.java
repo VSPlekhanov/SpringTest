b15a8f9cd4841fc54b9451b10b748b22d73760a0
Aleksandr Shevkunenko <aleksandr_shevkunenko@epam.com>
2017-10-12 17:37:15 +0300

    EPMLSTRSUM-161 Optimize model Question + Answer. Field AnswerAllFieldsDto#answerId eliminated because there's the same field in the parent class AnswerBaseDto.

--- model/src/main/java/com/epam/lstrsum/dto/answer/AnswerAllFieldsDto.java
+++ model/src/main/java/com/epam/lstrsum/dto/answer/AnswerAllFieldsDto.java
@@ -12,7 +12,7 @@ import java.time.Instant;
 @Setter
 @NoArgsConstructor
 public class AnswerAllFieldsDto extends AnswerBaseDto {
-    private String answerId;
+//    private String answerId;
     private QuestionBaseDto questionId;
 
     public AnswerAllFieldsDto(String text,
@@ -22,7 +22,7 @@ public class AnswerAllFieldsDto extends AnswerBaseDto {
             String answerId,
             QuestionBaseDto questionId) {
         super(answerId, text, createdAt, authorId, upVote);
-        this.answerId = answerId;
+//        this.answerId = answerId;
         this.questionId = questionId;
     }
 
@@ -34,14 +34,14 @@ public class AnswerAllFieldsDto extends AnswerBaseDto {
 
         AnswerAllFieldsDto that = (AnswerAllFieldsDto) o;
 
-        if (answerId != null ? !answerId.equals(that.answerId) : that.answerId != null) return false;
+        if (getAnswerId() != null ? !getAnswerId().equals(that.getAnswerId()) : that.getAnswerId() != null) return false;
         return questionId != null ? questionId.equals(that.questionId) : that.questionId == null;
     }
 
     @Override
     public int hashCode() {
         int result = super.hashCode();
-        result = 31 * result + (answerId != null ? answerId.hashCode() : 0);
+        result = 31 * result + (getAnswerId() != null ? getAnswerId().hashCode() : 0);
         result = 31 * result + (questionId != null ? questionId.hashCode() : 0);
         return result;
     }
