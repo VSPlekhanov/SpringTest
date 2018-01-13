0e91b34cb5882d010087a45ac6923084004f6280
Viacheslav Plekhanov <Viacheslav_Plekhanov@epam.com>
2018-01-13 21:28:16 +0300

    Added init method to FileLogger

--- src/main/java/test/spring/loggers/impl/FileEventLogger.java
+++ src/main/java/test/spring/loggers/impl/FileEventLogger.java
@@ -5,20 +5,27 @@ import test.spring.loggers.EventLogger;
 import test.spring.model.Event;
 
 import java.io.File;
+import java.io.IOException;
 
 public class FileEventLogger implements EventLogger {
 
-    private String filename;
+    private String fileName;
+    private File file;
+
+    public void init()throws IOException{
+        this.file = new File(fileName);
+        if (!file.canWrite()) throw new IOException("Can't write to file");
+    }
 
     public FileEventLogger(String filename) {
-        this.filename = filename;
+        this.fileName = filename;
     }
 
     @Override
     public void logEvent(Event event) {
         try {
-            FileUtils.writeStringToFile(new File(filename), event.toString(), "UTF-8", true);
-        } catch (Exception e) {
+            FileUtils.writeStringToFile(file, event.toString(), "UTF-8", true);
+        }catch (IOException e){
             e.printStackTrace();
         }
     }
